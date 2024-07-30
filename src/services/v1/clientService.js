d33710f219bdcd5be6fcb12a5ff85eac08c34351
Edu Moreno <e.m.morenolp@gmail.com>
2024-07-30 01:07:51 -0300

    fix import clienService

--- src/services/v1/clientService.js
+++ src/services/v1/clientService.js
@@ -1,5 +1,13 @@
 // src/services/clientService.js
-import * as clientModel from '../../models/v1/clientModel.prisma.js';
+import {
+    createClient,
+    deleteClient,
+    getClientById,
+    getClients,
+    loginClient,
+    updateClient
+} from '../../models/v1/clientModel.prisma.js';
+
 
 const formatClientResponse = (client) => {
     return {
@@ -14,30 +22,30 @@ const formatClientResponse = (client) => {
 };
 
 export const getClientsService = async (adminId) => {
-    const clients = await clientModel.getClients(adminId);
+    const clients = await getClients(adminId);
     return clients.map(formatClientResponse);
 };
 
 export const getClientByIdService = async (id, adminId) => {
-    const client = await clientModel.getClientById(id, adminId);
+    const client = await getClientById(id, adminId);
     return client ? formatClientResponse(client) : null;
 };
 
 export const createClientService = async (clientData, adminId) => {
-    const newClient = await clientModel.createClient(clientData, adminId);
+    const newClient = await createClient(clientData, adminId);
     return formatClientResponse(newClient);
 };
 
 export const updateClientService = async (id, clientData) => {
-    const updatedClient = await clientModel.updateClient(id, clientData);
+    const updatedClient = await updateClient(id, clientData);
     return formatClientResponse(updatedClient);
 };
 
 export const deleteClientService = async (id) => {
-    await clientModel.deleteClient(id);
+    await deleteClient(id);
 };
 
 export const loginClientService = async (email, password) => {
-    const client = await clientModel.loginClient(email, password);
+    const client = await loginClient(email, password);
     return client ? formatClientResponse(client) : null;
 };
